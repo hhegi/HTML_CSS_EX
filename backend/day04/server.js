8adf4ed6d9e6f9be096fab2fa4d4f5a92f77b5f9
hhegi <larbillilith@gmail.com>
2023-02-21 16:44:24 +0900

    0221 exfile add

--- backend/day04/server.js
+++ backend/day04/server.js
@@ -58,16 +58,33 @@ router.route("/home").get((req, res) => {
   res.end();
 });
 
+// 접속한 소켓을 저장하는 객체 준비
+const clientSocketMap = {};
+
 ////////
 // 클라이언트가 socket으로 접속하면 실행
 io.sockets.on("connection", (socket) => {
   console.log("/chat 소켓으로 접속 됨.");
+
   socket.on("login", function (data) {
-    console.log(data);
+    // console.log(data);
+    data.socketID = socket.id;
+    clientSocketMap[data.userID] = data;
+    let test01SocketID = clientSocketMap["test01"].socketID;
+    let test01Socket = io.sockets.sockets.get(test01SocketID);
+    test01Socket.emit("send message", "환영합니다");
   });
 
   socket.on("disconnect", function () {
     console.log("/chat 클라이언트 접속이 해제 됨.");
+  });
+
+  socket.on("sendChat", function (data) {
+    let test01SocketId = clientSocketMap[data.postID].socketID;
+    if (test01SocketId) {
+      let test01Socket = io.sockets.sockets.get(test01SocketId);
+      test01Socket.emit("send message", data);
+    }
   });
 });
 
